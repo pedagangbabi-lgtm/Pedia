e1656d6cb7f575e1e96853623f3051764e671f9c
Mosessniboy <samsonmosessimbolon@gmail.com>
2026-01-06 10:22:48 +0700

    midelwere login

--- middlewere.ts
+++ middlewere.ts
@@ -1,5 +1,18 @@
-export { auth as middleware } from "@/app/lib/auth";
+import { auth } from "@/app/lib/auth";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const session = await auth();
+
+  // ❌ Belum login
+  if (!session && req.nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ["/dashboard/:path*"], // halaman yang butuh login
+  matcher: ["/dashboard/:path*"],
 };
